b9590a62e25908975913de255463f4473165b98c
Riobard <me@riobard.com>
2015-02-08 15:29:05 -0330

    Added comment regarding RDT interface

--- NetworkSIM/simulation/admin.js
+++ NetworkSIM/simulation/admin.js
@@ -1,5 +1,18 @@
 function importRDT(rdt){
   // Import a replicated data type to use in the simulation
+
+  /* The RDT will have a common interface like the following after you import
+   * it:
+   *
+   *   rdt.init(networkIterator); // initiate the RDT type with your
+   *   networkIterator from the simulation
+   *
+   * For a simple integer counter RDT, it will also have methods to increment
+   * and get the value of the counter, e.g.:
+   *
+   *   rdt.inc(); // increment the integer counter by one
+   *   rdt.val(); // get the current value of the counter
+   */
 }
 
 function importApp(app){
