ce72943f103e4e9c6d8499a05de880100efcf153
Riobard <me@riobard.com>
2015-02-07 20:50:30 -0330

    Added comments to the interfaces

--- NetworkSIM/simulation/admin.js
+++ NetworkSIM/simulation/admin.js
@@ -1,27 +1,23 @@
-/**
- * New node file
- */
-
-function importRTD(rdtName){
-	
+function importRDT(rdt){
+  // Import a replicated data type to use in the simulation
 }
 
-function importApp(appName){
-	
+function importApp(app){
+  // Import and initialize your web application in the simulation
 }
 
-function addNetwork(networkName,networkKind){
-	
+function addNetwork(networkName, networkKind){
+  // Add a network with the given name and kind to the simulation
 }
 
 function removeNetwork(networkName){
-	
+  // Remove a network with the given name from the simulation
 }
 
 function addDevice(deviceName){
-	
+  // Add a device with the given name to the simulation
 }
 
 function removeDevice(deviceName){
-	
+  // Remove a device with the given name from the simulation
 }
--- NetworkSIM/simulation/network_topology.js
+++ NetworkSIM/simulation/network_topology.js
@@ -1,64 +1,106 @@
-/**
- * New node file
- */
-
 var networkList;
 var deviceList;
 
+
 function NetworkIterator(){
-	this.first = function() {
-    };
-    
-    this.next = function() {
-    };
-    
-    this.hasNext = function() {
-    };
-    
-    this.reset = function() {
-    };
-    
-    this.each = function(callback) {
-        for (var item = this.first(); this.hasNext(); item = this.next()) {
-            callback(item);
-        }
-    };
+  // An iterator over networks in the simulation
+
+  this.first = function() {
+    // Return the first element
+  };
+
+  this.next = function() {
+    // Return the next element
+  };
+
+  this.hasNext = function() {
+    // Determine if there are more elements to iterate
+  };
+
+  this.reset = function() {
+    // Reset the iterator to its initial state so it can be re-used
+  };
+
+  this.each = function(callback) {
+    // Invoke the callback function on each element
+    for (var item = this.first(); this.hasNext(); item = this.next()) {
+      callback(item);
+    }
+  };
 }
+
 
 function DeviceIterator(){
-	this.first = function() {
-    };
-    
-    this.next = function() {
-    };
-    
-    this.hasNext = function() {
-    };
-    
-    this.reset = function() {
-    };
-    
-    this.each = function(callback) {
-    };
+  // Similar to the NetworkIterator except the elements are devices
+
+  this.first = function() {
+  };
+
+  this.next = function() {
+  };
+
+  this.hasNext = function() {
+  };
+
+  this.reset = function() {
+  };
+
+  this.each = function(callback) {
+  };
 }
 
-function Network(networkName,networkKind){
-	this.deviceIterator = new DeviceIterator(); // Returns an iterator that provides Device objects
-	this.addDevice = function(device){}; // Adds a new device to the network
-	this.removeDevice = function(device){}; //
-	this.connectNetwork = function(network){};
-	this.disconnectNetwork = function(network){};
-	this.networkName = networkName; // String
-	this.networkKind = networkKind; // Constant: WiFi, GSM
+
+function Network(networkName, networkKind){
+  // Construct a network object
+
+  this.networkName = networkName; // String
+  this.networkKind = networkKind; // Constant: WiFi, GSM
+
+  this.deviceIterator = new DeviceIterator(); // Returns an iterator that provides Device objects
+
+  this.addDevice = function(device){
+    // Adds a device object to the network
+  };
+
+  this.removeDevice = function(device){
+    // Remove the device object from the network
+  };
+
+  this.connectNetwork = function(network){
+    // Connect the network to another
+  };
+
+  this.disconnectNetwork = function(network){
+    // Disconnect the network from another
+  };
 }
+
 
 function Device(deviceName){
-	this.joinNetwork = function(network){};
-	this.leaveNetwork = function(){};
-	this.returnNetwork = function(){};
-	this.replicateRDT = function(rdt){}; 
-	this.accessRDT = function(){};
+  // Construct a device object
+
+  this.deviceName = deviceName;
+
+  this.joinNetwork = function(network){
+    // Make the device join a network
+  };
+
+  this.leaveNetwork = function(){
+    // Make the device leave connected network
+  };
+
+  this.returnNetwork = function(){
+    // Make the device re-join a previous network
+  };
+
+  this.replicateRDT = function(rdt){
+    // Register a replicated data type in the device
+  };
+
+  this.accessRDT = function(){
+    // Access the previously registered replicated data type in the device
+  };
 }
 
-exports.NetworkIterator = NetworkIterator;
 
+exports.NetworkIterator = NetworkIterator;
