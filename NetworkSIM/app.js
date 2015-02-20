27f28a241363f7b11d9ebc7023f9a713e031fca6
db2763 <db2763@mun.ca>
2015-02-20 00:03:37 -0330

    Began adding functionality to pages. public/js scripts are loaded in head of html (hjs) files; they send requests which are routed in app.js to the appropriate function in simulation js files (admin or registerMobileDevices). All very untested and buggy.

--- NetworkSIM/app.js
+++ NetworkSIM/app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -7,9 +6,9 @@ var express = require('express');
 var routes = require('./routes');
 var admin = require('./simulation/admin');
 var registerMobileDevice = require('./simulation/registerMobileDevice'); // added
-																			// for
-																			// token
-																			// distribution
+// for
+// token
+// distribution
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
@@ -18,8 +17,9 @@ var morgan = require('morgan');// formerly logger
 var bodyParser = require('body-parser');
 var methodOverride = require("method-override");
 var mongo = require("mongojs");
-var expressMail = require('express-mail'); // added to distribute token by e-mail
-										// in registerMobileDevice
+var expressMail = require('express-mail'); // added to distribute token by
+// e-mail
+// in registerMobileDevice
 
 var app = express();
 
@@ -37,75 +37,122 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 // development only
 if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 // Copied and pasted from express-mailer example
 // Might need to adjust
 expressMail.extend(app, {
-	  from: 'no-reply@example.com',
-	  host: 'smtp.gmail.com', // hostname
-	  secureConnection: true, // use SSL
-	  port: 465, // port for secure SMTP
-	  transportMethod: 'SMTP', // default is SMTP. Accepts anything that
-								// nodemailer accepts
-	  auth: {
-	    user: 'gmail.user@gmail.com',
-	    pass: 'userpass'
-	  }
-	});
-
-
+	from : 'no-reply@example.com',
+	host : 'smtp.gmail.com', // hostname
+	secureConnection : true, // use SSL
+	port : 465, // port for secure SMTP
+	transportMethod : 'SMTP', // default is SMTP. Accepts anything that
+	// nodemailer accepts
+	auth : {
+		user : 'gmail.user@gmail.com',
+		pass : 'userpass'
+	}
+});
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-// Move callbacks to routes
-app.post('/addNetworkToSimulation', function (req, res) {
+app.post('/importRDT', function(req, res) {
+	admin.importRDT(req.body.name);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
+});
+
+app.post('/removeRDT', function(req, res) {
+	admin.removeRDT(req.body.name);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
+});
+
+app.post('/importApp', function(req, res) {
+	admin.importApp(req.body.name);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
+});
+
+app.post('/removeApp', function(req, res) {
+	admin.removeApp(req.body.name);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
+});
+
+app.post('/addNetwork', function(req, res) {
 	admin.addNetwork(req.body.networkName, req.body.networkKind);
-	res.status(200);
-	res.end(); 
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
 });
-app.post('/removeNetworkFromSimulation', function (req, res) {
-	admin.removeNetwork(req.body.networkName); 
-	res.status(200);
-	res.end(); 
+
+app.post('/removeNetwork', function(req, res) {
+	admin.removeNetwork(req.body.name);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
 });
-app.post('/addDeviceToSimulation', function (req, res) {
-	admin.addDevice(req.body.deviceName);
-	res.status(200);
-	res.end(); 
+app.post('/addDevice', function(req, res) {
+	admin.addDevice(req.body.name);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
 });
-app.post('/removeDeviceFromSimulation', function (req, res) {
-	admin.removeDevice(req.body.deviceName); 
-	res.status(200);
-	res.end(); 
+app.post('/removeDevice', function(req, res) {
+	admin.removeDevice(req.body.name);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
 });
-app.post('/addDeviceToNetwork', function (req, res) {
-	admin.addDeviceToNetwork(req.body.network, req.body.device); 
-	res.status(200);
-	res.end(); 
+app.post('/addDeviceToNetwork', function(req, res) {
+	admin.addDeviceToNetwork(req.body.networkName, req.body.deviceName);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
 });
-// etc. for removeFrom, connecting two networks, etc. Add after testing.
+
+app.post('/removeDeviceFromNetwork', function(req, res) {
+	admin.removeDeviceFromNetwork(req.body.networkName, req.body.deviceName);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
+});
+
+app.post('/connectTwoNetworks', function(req, res) {
+	admin.connectTwoNetworks(req.body.network1Name, req.body.network1Name);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
+});
+
+app.post('/disconnectTwoNetworks', function(req, res) {
+	admin.disconnectTwoNetworks(req.body.network1Name, req.body.network1Name);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
+});
+
+app.post('/removeDeviceFromCurrentNetwork', function(req, res) {
+	admin.removeDeviceFromCurrentNetwork(req.body.name);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
+});
+
+app.post('/returnDeviceToPreviousNetwork', function(req, res) {
+	admin.returnDeviceToPreviousNetwork(req.body.name);
+	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
+});
 
 app.post('/distributeToken', function(req, res) {
-	registerMobileDevice.distributeToken(req.body.emailAddress); 
+	registerMobileDevice.distributeToken(req.body.emailAddress);
 	res.status(200);
-	res.end(); 	
+	res.end();
 });
 
-app.post('/submitToken', function(req, res) {
-	var success = registerMobileDevice.registerWithToken(req.body.token); 
+app.post('/registerWithToken', function(req, res) {
+	var success = registerMobileDevice.registerWithToken(req.body.token);
 	if (success) {
-		res.status(200); 
-		res.end(); 
-	}
-	else {
+		res.status(200);
+		res.end();
+	} else {
 		res.status(400);
-		res.end(); 
+		res.end();
 	}
-}); 
+});
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.post('/validateToken', function(req, res) {
+	var valid = registerMobileDevice.validateToken(req.body.token);
+	if (success) {
+		res.status(200);
+		res.end();
+	} else {
+		res.status(400);
+		res.end();
+	}
+});
+
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
 });
 //
--- NetworkSIM/simulation/registerMobileDevice.js
+++ NetworkSIM/simulation/registerMobileDevice.js
@@ -9,6 +9,37 @@ var dcoll = db.collection('devices');
 var app = require('../app').app; // use app.mailer to send e-mail
 
 /**
+ * 1. Get reference to one unassigned device 2. Get that device's token (id) 3.
+ * User express-mailer module to send e-mail. The body is the token_email.hjs
+ * template, configured to use the same view engine.
+ */
+function distributeToken(emailAddress) {
+
+	var unassignedDevice = dcoll.findOne({
+		assigned : false
+	});
+	var hexstring = unassignedDevice._id.valueOf(); // valueOf method returns
+	// str attribute of ObjectId
+	// object that is value of
+	// _id attribute of device
+	// object
+	app.mailer.send('token_email', {
+		to : emailAddress, // REQUIRED. This can be a comma delimited string
+		// just like a normal email to field.
+		bcc : 'db2763@mun.ca, kn8384@mun.ca, xx1100@mun.ca',
+		subject : 'Network Simulation Registration', // REQUIRED.
+		registerURL : 'http://sc-4.cs.mun.ca/register', // for Hogan!
+		token : hexstring
+	// for Hogan
+	}, function(err) {
+		if (err) {
+			console.log('There was an error sending the token e-mail');
+			return;
+		}
+	});
+}
+
+/**
  * Attempt to a find a device in the table whose id matches the token the user
  * has submitted --- see admin.addDevice comment for details. If found, set the
  * assigned attribute to true. Update returns a writeResult object. If a device
@@ -42,82 +73,26 @@ function registerWithToken(token) {
 	}
 }
 
-/**
- * 1. Get reference to one unassigned device 2. Get that device's token (id) 3.
- * User express-mailer module to send e-mail. The body is the token_email.hjs
- * template, configured to use the same view engine.
- */
-function distributeToken(emailAddress) {
-	var unassignedDevice = dcoll.findOne({
-		assigned : false
+function validateToken(token) {
+
+	var doc = dcoll.findOne({
+		"_id.str" : token,
+		assigned : true
 	});
-	var hexstring = unassignedDevice._id.valueOf(); // valueOf method returns
-	// str attribute of ObjectId
-	// object that is value of
-	// _id attribute of device
-	// object
-	app.mailer.send('token_email', {
-		to : emailAddress, // REQUIRED. This can be a comma delimited string
-		// just like a normal email to field.
-		bcc : 'db2763@mun.ca, kn8384@mun.ca, xx1100@mun.ca',
-		subject : 'Network Simulation Registration', // REQUIRED.
-		registerURL : 'http://sc-4.cs.mun.ca/register', // for Hogan!
-		token : hexstring //for Hogan
-	}, function(err) {
-		if (err) {
-			console.log('There was an error sending the token e-mail');
-			return;
-		}
-	});
+
+	var valid;
+
+	if (doc) { // found an assigned Device with this token
+		valid = true;
+	}
+
+	else {
+		valid = false;
+	}
+
+	return valid;
 }
 
 exports.registerWithToken = registerWithToken;
 exports.distributeToken = distributeToken;
-
-// From API
-/*
- * Sending an email You can send an email by calling app.mailer.send(template,
- * locals, callback). To send an email using the template above you could write:
- * 
- * app.get('/', function (req, res, next) { app.mailer.send('email', { to:
- * 'example@example.com', // REQUIRED. This can be a comma delimited string just
- * like a normal email to field. subject: 'Test Email', // REQUIRED.
- * otherProperty: 'Other Property' // All additional properties are also passed
- * to the template as local variables. }, function (err) { if (err) { // handle
- * error console.log(err); res.send('There was an error sending the email');
- * return; } res.send('Email Sent'); }); }); You can also send an email by
- * calling mailer on an applications response object: res.mailer.send(template,
- * options, callback).
- * 
- * Nodemailer options It is also possible to change the options supplied to
- * nodemailers sendMail function. Instead of passing in the template name to
- * mailer.send or mailer.render you can pass an object with any of the following
- * fields:
- * 
- * template - REQUIRED - The name of the template to render from - The e-mail
- * address of the sender. All e-mail addresses can be plain sender@server.com or
- * formatted Sender Name <sender@server.com> to - Comma separated list or an
- * array of recipients e-mail addresses that will appear on the To: field cc -
- * Comma separated list or an array of recipients e-mail addresses that will
- * appear on the Cc: field bcc - Comma separated list or an array of recipients
- * e-mail addresses that will appear on the Bcc: field replyTo - An e-mail
- * address that will appear on the Reply-To: field inReplyTo - The message-id
- * this message is replying references - Message-id list subject - The subject
- * of the e-mail headers - An object of additional header fields {"X-Key-Name":
- * "key value"} (NB! values are passed as is, you should do your own encoding to
- * 7bit and folding if needed) attachments - An array of attachment objects.
- * alternatives - An array of alternative text contents (in addition to text and
- * html parts) envelope - optional SMTP envelope, if auto generated envelope is
- * not suitable messageId - optional Message-Id value, random value will be
- * generated if not set. Set to false to omit the Message-Id header date -
- * optional Date value, current UTC string will be used if not set encoding -
- * optional transfer encoding for the textual parts (defaults to
- * "quoted-printable") charset - optional output character set for the textual
- * parts (defaults to "utf-8") dsn - An object with methods success, failure and
- * delay. If any of these are set to true, DSN will be used For example you
- * could cc others with the previous example like this:
- * 
- * app.mailer.send( { template: 'email', // REQUIRED cc: 'cc@example.com' }, {
- * to: 'example@example.com', subject: 'Test Email', otherProperty: 'Other
- * Property' }, function (err) { if (err) { // handle error }; // mail sent! } );
- */
+exports.validateToken = validateToken;
