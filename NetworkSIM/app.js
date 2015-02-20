52cfde30f66f3aecaedebe22c86b951e56056fa5
db2763 <db2763@mun.ca>
2015-02-20 05:22:52 -0330

    Refactored router to put routes in js files in routes directory, thus tidying up app.js

--- NetworkSIM/app.js
+++ NetworkSIM/app.js
@@ -1,32 +1,27 @@
-/**
- * Module dependencies.
- */
-
+// ------------------- MODULE DEPENDENCIES ---------------
 var express = require('express');
-var routes = require('./routes');
-var admin = require('./simulation/admin');
-var registerMobileDevice = require('./simulation/registerMobileDevice'); // added
-// for
-// token
-// distribution
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 // var favicon = require('favicon');
-var morgan = require('morgan');// formerly logger
+var morgan = require('morgan'); // formerly logger
 var bodyParser = require('body-parser');
 var methodOverride = require("method-override");
 var mongo = require("mongojs");
-var expressMail = require('express-mail'); // added to distribute token by
-// e-mail
-// in registerMobileDevice
+var expressMail = require('express-mail'); // NOT WORKING
+
+// route modules
+var indexroutes = require('./routes/index');
+var userroutes = require('./routes/user');
+var nsroutes = require('./routes/network_settings_routes');
+var rmdroutes = require('./routes/registerMobileDevice_routes');
+
+// ------------------- SET UP ------------------------------
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-// app.set('simulation', path.join(__dirname, 'simulation'));
 app.set('view engine', 'hjs');
 // app.use(favicon());
 app.use(morgan("dev")); // (formerly known as 'logger')
@@ -39,8 +34,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('development' === app.get('env')) {
 	app.use(express.errorHandler());
 }
+
 // Copied and pasted from express-mailer example
-// Might need to adjust
+// NOT WORKING
 expressMail.extend(app, {
 	from : 'no-reply@example.com',
 	host : 'smtp.gmail.com', // hostname
@@ -54,105 +50,36 @@ expressMail.extend(app, {
 	}
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+// -------------------- ROUTING ----------------------------
 
-app.post('/importRDT', function(req, res) {
-	admin.importRDT(req.body.name);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
+app.get('/', indexroutes.indexRoute);
+app.get('/users', userroutes.list);
 
-app.post('/removeRDT', function(req, res) {
-	admin.removeRDT(req.body.name);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
+// network_settings
+app.post('/importRDT', nsroutes.importRDTRoute);
+app.post('/removeRDT', nsroutes.removeRDTRoute);
+app.post('/importApp', nsroutes.importAppRoute);
+app.post('/removeApp', nsroutes.removeAppRoute);
+app.post('/addNetwork', nsroutes.addNetworkRoute);
+app.post('/removeNetwork', nsroutes.removeNetworkRoute);
+app.post('/addDevice', nsroutes.addDeviceRoute);
+app.post('/removeDevice', nsroutes.removeDeviceRoute);
+app.post('/addDeviceToNetwork', nsroutes.addDeviceToNetworkRoute);
+app.post('/removeDeviceFromNetwork', nsroutes.removeDeviceFromNetworkRoute);
+app.post('/connectTwoNetworks', nsroutes.connectTwoNetworksRoute);
+app.post('/disconnectTwoNetworks', nsroutes.disconnectTwoNetworksRoute);
+app.post('/removeDeviceFromCurrentNetwork',
+		nsroutes.removeDeviceFromCurrentNetworkRoute);
+app.post('/returnDeviceToPreviousNetwork',
+		nsroutes.returnDeviceToPreviousNetworkRoute);
 
-app.post('/importApp', function(req, res) {
-	admin.importApp(req.body.name);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
+// registerMobileDevice
+app.post('/distributeToken', rmdroutes.distributeTokenRoute);
+app.post('/registerWithToken', rmdroutes.registerWithTokenRoute);
+app.post('/validateToken', rmdroutes.validateTokenRoute);
 
-app.post('/removeApp', function(req, res) {
-	admin.removeApp(req.body.name);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
-
-app.post('/addNetwork', function(req, res) {
-	admin.addNetwork(req.body.networkName, req.body.networkKind);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
-
-app.post('/removeNetwork', function(req, res) {
-	admin.removeNetwork(req.body.name);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
-app.post('/addDevice', function(req, res) {
-	admin.addDevice(req.body.name);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
-app.post('/removeDevice', function(req, res) {
-	admin.removeDevice(req.body.name);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
-app.post('/addDeviceToNetwork', function(req, res) {
-	admin.addDeviceToNetwork(req.body.networkName, req.body.deviceName);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
-
-app.post('/removeDeviceFromNetwork', function(req, res) {
-	admin.removeDeviceFromNetwork(req.body.networkName, req.body.deviceName);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
-
-app.post('/connectTwoNetworks', function(req, res) {
-	admin.connectTwoNetworks(req.body.network1Name, req.body.network1Name);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
-
-app.post('/disconnectTwoNetworks', function(req, res) {
-	admin.disconnectTwoNetworks(req.body.network1Name, req.body.network1Name);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
-
-app.post('/removeDeviceFromCurrentNetwork', function(req, res) {
-	admin.removeDeviceFromCurrentNetwork(req.body.name);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
-
-app.post('/returnDeviceToPreviousNetwork', function(req, res) {
-	admin.returnDeviceToPreviousNetwork(req.body.name);
-	res.render('network_settings.hjs', admin.getHoganTemplateVariables());
-});
-
-app.post('/distributeToken', function(req, res) {
-	registerMobileDevice.distributeToken(req.body.emailAddress);
-	res.status(200);
-	res.end();
-});
-
-app.post('/registerWithToken', function(req, res) {
-	var success = registerMobileDevice.registerWithToken(req.body.token);
-	if (success) {
-		res.status(200);
-		res.end();
-	} else {
-		res.status(400);
-		res.end();
-	}
-});
-
-app.post('/validateToken', function(req, res) {
-	var valid = registerMobileDevice.validateToken(req.body.token);
-	if (success) {
-		res.status(200);
-		res.end();
-	} else {
-		res.status(400);
-		res.end();
-	}
-});
+// ----------------------- SERVER --------------------------------------
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
-//
--- NetworkSIM/routes/index.js
+++ NetworkSIM/routes/index.js
@@ -1,8 +1,9 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.indexRoute = function(req, res) {
+	res.render('index', {
+		title : 'Express'
+	});
 };
