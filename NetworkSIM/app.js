f62ab0cb74036dbc074b3ef44fbbc7e144a7ba8b
db2763 <db2763@mun.ca>
2015-02-20 13:26:07 -0330

    Hopefully everything is synced back up.

--- NetworkSIM/app.js
+++ NetworkSIM/app.js
@@ -8,15 +8,14 @@ var morgan = require('morgan'); // formerly logger
 var bodyParser = require('body-parser');
 var methodOverride = require("method-override");
 var mongo = require("mongojs");
-var nodemailer = require("nodemailer"); // added to distribute token by e-mail
-										// in registerMobileDevice
-//var expressMail = require('express-mail'); // NOT WORKING
+var nodemailer = require("nodemailer");
+
+// route modules
 var indexroutes = require('./routes/index_routes');
 var userroutes = require('./routes/user');
 var nsroutes = require('./routes/network_settings_routes');
 var rmdroutes = require('./routes/registerMobileDevice_routes');
 var cApproutes = require('./routes/counterApp_routes');
-
 
 var app = express();
 
@@ -39,11 +38,11 @@ if ('development' === app.get('env')) {
 // Copied and pasted from express-mailer example
 // Might need to adjust
 var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'gmail.user@gmail.com',
-        pass: 'userpass'
-    }
+	service : 'Gmail',
+	auth : {
+		user : 'gmail.user@gmail.com',
+		pass : 'userpass'
+	}
 });
 
 // NB! No need to recreate the transporter object. You can use
@@ -51,89 +50,28 @@ var transporter = nodemailer.createTransport({
 
 // setup e-mail data with unicode symbols
 var mailOptions = {
-    from: 'Fred Foo ✔ <foo@blurdybloop.com>', // sender address
-    to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world ✔', // plaintext body
-    html: '<b>Hello world ✔</b>' // html body
+	from : 'Fred Foo ✔ <foo@blurdybloop.com>', // sender address
+	to : 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+	subject : 'Hello ✔', // Subject line
+	text : 'Hello world ✔', // plaintext body
+	html : '<b>Hello world ✔</b>' // html body
 };
 
 // send mail with defined transport object
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        console.log(error);
-    }else{
-        console.log('Message sent: ' + info.response);
-    }
-});
-
-
-
-app.get('/', indexroutes.indexRoute);
-app.get('/users', indexroutes.userHomeRoute);
-
-// Move callbacks to routes
-app.post('/addNetworkToSimulation', function (req, res) {
-	admin.addNetwork(req.body.networkName, req.body.networkKind);
-	res.status(200);
-	res.end(); 
-});
-app.post('/removeNetworkFromSimulation', function (req, res) {
-	admin.removeNetwork(req.body.networkName); 
-	res.status(200);
-	res.end(); 
-});
-app.post('/addDeviceToSimulation', function (req, res) {
-	admin.addDevice(req.body.deviceName);
-	res.status(200);
-	res.end(); 
-});
-app.post('/removeDeviceFromSimulation', function (req, res) {
-	admin.removeDevice(req.body.deviceName); 
-	res.status(200);
-	res.end(); 
-});
-app.post('/addDeviceToNetwork', function (req, res) {
-	admin.addDeviceToNetwork(req.body.network, req.body.device); 
-	res.status(200);
-	res.end(); 
-});
-// etc. for removeFrom, connecting two networks, etc. Add after testing.
-
-app.post('/distributeToken', function(req, res) {
-	registerMobileDevice.distributeToken(req.body.emailAddress); 
-	res.status(200);
-	res.end(); 	
-});
-
-app.post('/submitToken', function(req, res) {
-	var success = registerMobileDevice.registerWithToken(req.body.token); 
-	if (success) {
-		res.status(200); 
-		res.end();
+transporter.sendMail(mailOptions, function(error, info) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Message sent: ' + info.response);
 	}
 });
-// NOT WORKING
-/*expressMail.extend(app, {
-	from : 'no-reply@example.com',
-	host : 'smtp.gmail.com', // hostname
-	secureConnection : true, // use SSL
-	port : 465, // port for secure SMTP
-	transportMethod : 'SMTP', // default is SMTP. Accepts anything that
-	// nodemailer accepts
-	auth : {
-		user : 'gmail.user@gmail.com',
-		pass : 'userpass'
-	}
-});*/
 
 // -------------------- ROUTING ----------------------------
-
 app.get('/', indexroutes.indexRoute);
 app.get('/userHome', indexroutes.userHomeRoute);
-app.get('/adminHome', indexroutes.adminHomeRoute);
+app.get('/adminHome', indexroute.adminHomeRoute);
 app.get('/users', userroutes.list);
-app.get('/networkSettings', indexroutes.networkSettings);
+
 // network_settings
 app.post('/importRDT', nsroutes.importRDTRoute);
 app.post('/removeRDT', nsroutes.removeRDTRoute);
@@ -156,6 +94,7 @@ app.post('/returnDeviceToPreviousNetwork',
 app.post('/distributeToken', rmdroutes.distributeTokenRoute);
 app.post('/registerWithToken', rmdroutes.registerWithTokenRoute);
 app.post('/validateToken', rmdroutes.validateTokenRoute);
+app.post('/adminLogin', rmdroutes.adminLoginRoute);
 
 // counter app
 app.get("/counterApp", cApproutes.counterAppRoute);
